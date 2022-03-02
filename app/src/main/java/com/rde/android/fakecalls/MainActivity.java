6388b33abe27c95e4563c48b2c92d48837814a4b
rodolfo <rodolfothegreat@hotmail.com>
2022-03-02 22:23:02 +1000

    Migrated to Androidx, also now it works with ADS Bumblebee

--- app/src/main/java/com/rde/android/fakecalls/MainActivity.java
+++ app/src/main/java/com/rde/android/fakecalls/MainActivity.java
@@ -6,10 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
